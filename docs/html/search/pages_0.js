69175a0a0de6e770be6bbe796a608676cab3f9d3
cbene <cbene@kth.se>
2026-05-06 17:46:54 +0200

    update docs

--- docs/html/search/pages_0.js
+++ docs/html/search/pages_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['c_2b_2b_20json_20parser_96',['C++ Json Parser',['../index.html',1,'']]]
+  ['c_2b_2b_20json_20parser_20_3cimg_20src_3d_22https_3a_2f_2fimg_2eshields_2eio_2fbadge_2fc_252b_252b_2d20_2dblue_22_20alt_3d_22c_2b_2b20_22_2f_3e_96',['C++ Json Parser &lt;img src=&quot;https://img.shields.io/badge/C%2B%2B-20-blue&quot; alt=&quot;C++20&quot;/&gt;',['../index.html',1,'']]]
 ];
